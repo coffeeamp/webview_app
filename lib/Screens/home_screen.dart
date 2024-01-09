a9817993de5ce06d6c8e1bea30f1bddcad34e957
coffeeamp <83232889+coffeeamp@users.noreply.github.com>
2024-01-09 23:57:39 +0900

    webview

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,27 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-final homeUrl = Uri.parse('https://www.google.com');
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
-class HomeScreen extends StatelessWidget {
-  late final WebViewController controller = WebViewController();
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
-  HomeScreen({super.key});
+class _HomeScreenState extends State<HomeScreen> {
+  late WebViewController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://flutter.dev'));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green[400],
-        title: Text('webview_app',
-          style: TextStyle(color: Colors.white)),
-        centerTitle: true,
-      ),
-      body: (
-        WebViewWidget(
-          controller: controller,
-        )
-      ),
+    final Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: size.width,
+      height: size.height,
+      child: WebViewWidget(controller: controller),
     );
   }
 }
