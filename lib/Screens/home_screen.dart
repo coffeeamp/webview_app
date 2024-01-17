5321f1ca44927169f96ab8e941321ccf33b4a7d6
coffeeamp <83232889+coffeeamp@users.noreply.github.com>
2024-01-18 01:19:46 +0900

    callback_webview

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+const homeUrl = 'https://www.google.co.kr/?hl=ko';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -16,32 +18,43 @@ class _HomeScreenState extends State<HomeScreen> {
     controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            // Update loading bar.
-          },
-          onPageStarted: (String url) {},
-          onPageFinished: (String url) {},
-          onWebResourceError: (WebResourceError error) {},
-          onNavigationRequest: (NavigationRequest request) {
-            if (request.url.startsWith('https://www.youtube.com/')) {
-              return NavigationDecision.prevent;
-            }
-            return NavigationDecision.navigate;
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse('https://flutter.dev'));
+      // ..setNavigationDelegate(
+      //   NavigationDelegate(
+      //     onProgress: (int progress) {
+      //       // Update loading bar.
+      //     },
+      //     onPageStarted: (String url) {},
+      //     onPageFinished: (String url) {},
+      //     onWebResourceError: (WebResourceError error) {},
+      //     onNavigationRequest: (NavigationRequest request) {
+      //       if (request.url.startsWith('https://www.youtube.com/')) {
+      //         return NavigationDecision.prevent;
+      //       }
+      //       return NavigationDecision.navigate;
+      //     },
+      //   ),
+      // )
+      ..loadRequest(Uri.parse(homeUrl));
   }
 
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-    return SizedBox(
-      width: size.width,
-      height: size.height,
-      child: WebViewWidget(controller: controller),
+//    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Webview', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: (){
+              controller.loadRequest(Uri.parse(homeUrl));            },
+            icon: Icon(Icons.home),
+          )
+        ],
+      ),
+      body: WebViewWidget(
+        controller: controller),
     );
   }
 }
